47e6fff426ef2b539719e3b5312283b64e57a22d
wangz <jitong2326@163.com>
2022-08-24 16:43:07 +0800

    侧边菜单 轮播图组件

---
+++ src/components/sideMenu/SideMenu.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { sideMenuList } from "./mockup";
+import styles from "./SideMenu.module.css";
+import { Menu } from "antd";
+import { GifOutlined } from "@ant-design/icons";
+
+export const SideMenu: React.FC = () => {
+  return (
+    <Menu mode="vertical" className={styles["side-menu"]}>
+      {sideMenuList.map((e, i) => {
+        return (
+          <Menu.SubMenu
+            key={`side-menu-${i}`}
+            title={
+              <span>
+                <GifOutlined />
+                {e.title}
+              </span>
+            }
+          >
+            {e.subMenu.map((se, si) => {
+              return (
+                <Menu.SubMenu
+                  key={`sub-menu-${si}`}
+                  title={
+                    <span>
+                      <GifOutlined />
+                      {se.title}
+                    </span>
+                  }
+                >
+                  {se.subMenu.map((sse, ssi) => {
+                    return (
+                      <Menu.Item key={`sub-sub-menu-${ssi}`}>
+                        <span>
+                          <GifOutlined />
+                          {sse}
+                        </span>
+                      </Menu.Item>
+                    );
+                  })}
+                </Menu.SubMenu>
+              );
+            })}
+          </Menu.SubMenu>
+        );
+      })}
+    </Menu>
+  );
+};
