27fc11146f7eb31bbaa91f51d43a99e24f158e4f
wangz <jitong2326@163.com>
2022-08-25 17:33:35 +0800

    多页面路由

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,11 @@ function App() {
   return (
     <div className={styles.App}>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />}></Route>
-        </Routes>
+          <Routes>
+            <Route path="/" element={<HomePage />}></Route>
+            <Route path="/signIn" element={<h1>11111111</h1>}></Route>
+            <Route path="*" element={<h1>404</h1>}></Route>
+          </Routes>
       </BrowserRouter>
     </div>
   );
